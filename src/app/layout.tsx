726ffe755632755ad4b7c9429a1f0fa89ad538e1
ShafaqMughal <159832153+SAAD-Qf@users.noreply.github.com>
2025-01-30 03:14:58 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Providers from "@/app/Providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: [] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
+        <Providers>
+      <Header/>
         {children}
-      </body>
+        <Footer/>
+        </Providers>
+        </body>
     </html>
   );
 }
